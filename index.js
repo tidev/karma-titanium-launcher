b3752f7d94a24473092fdee9ac13f1faa7e6bc84
Chris Williams <chris.a.williams@gmail.com>
2019-04-26 10:07:09 -0400

    style: fix lint issues

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const ProjectManager = require('./lib/project-manager');
 const TitaniumLauncher = require('./lib/titanium-launcher');
 
 module.exports = {
-    'projectManager': ['type', ProjectManager],
-    'launcher:Titanium': ['type', TitaniumLauncher]
+	projectManager: [ 'type', ProjectManager ],
+	'launcher:Titanium': [ 'type', TitaniumLauncher ]
 };
